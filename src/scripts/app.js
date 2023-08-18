16796827f47a2e675cc770285b0820ec8f20ea25
SmiKhyyyyz <sylvain.dvx@icloud.com>
2023-08-18 20:11:20 +0200

    footer

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,36 +4,47 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 gsap.to("#rencontres", {
   x: 0,
+  opacity: 1,
+  duration: 1,
   scrollTrigger: {
       trigger: "#rencontres",
   },
 });
 gsap.to("#problemes", {
   x: 0,
+  opacity: 1,
+  duration: 1,
   scrollTrigger: {
       trigger: "#problemes",
   },
 });
 gsap.to("#hypothese", {
   x: 0,
+  opacity: 1,
+  duration: 1,
   scrollTrigger: {
       trigger: "#hypothese",
   },
 });
 gsap.to("#solution", {
   x: 0,
+  opacity: 1,
+  duration: 1,
   scrollTrigger: {
       trigger: "#solution",
   },
 });
 gsap.to("#recherches", {
   x: 0,
+  opacity: 1,
   scrollTrigger: {
       trigger: "#recherches",
   },
 });
 gsap.to("#presentation", {
   x: 0,
+  opacity: 1,
+  duration: 1,
   scrollTrigger: {
       trigger: "#presentation",
   },
